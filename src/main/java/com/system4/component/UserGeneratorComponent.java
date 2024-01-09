752a868d1b08cb16bf46cab7e15823615324144e
MichalD <michaeld13@interia.eu>
2024-01-09 09:42:00 +0100

    saving users to file and database on start

--- src/main/java/com/system4/component/UserGeneratorComponent.java
+++ src/main/java/com/system4/component/UserGeneratorComponent.java
@@ -1,7 +1,57 @@
 package com.system4.component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.system4.dto.UserDTO;
+import com.system4.dto.UsersDTO;
+import com.system4.factory.UserFactory;
+import com.system4.model.User;
+import com.system4.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public interface UserGeneratorComponent {
-    UserDTO generateUser();
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserGeneratorComponent {
+    private final UserFactory userFactory;
+    private final UserRepository repository;
+    private final ObjectMapper objectMapper;
+    @Autowired
+    public UserGeneratorComponent(UserFactory userFactory, UserRepository repository) {
+        this.userFactory = userFactory;
+        this.repository = repository;
+        objectMapper = new ObjectMapper();
+    }
+    @PostConstruct
+    public void postConstruct() throws IOException {
+        if (repository.count() > 0) {
+            return;
+        }
+        System.out.println("UserGeneratorComponent initialized");
+        List<User> users = generateUsers();
+        repository.saveAll(users);
+        List<UserDTO> usersDTOs = users.stream()
+                        .map(user -> new UserDTO(user.getName(),user.getSurname(),user.getLogin()))
+                                .toList();
+        objectMapper.writeValue(new File("generatedUsers.json"), new UsersDTO(usersDTOs));
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("UserGeneratorComponent destroyed");
+    }
+
+    public List<User> generateUsers(){
+        List<User> users = new ArrayList<>();
+        for(int i = 0; i < 50_000; i++){
+            User user = userFactory.createUser();
+            users.add(user);
+        }
+        return users;
+    }
 }
